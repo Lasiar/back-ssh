9084753ec5ca430b597973fd2ad688df07386551
Lasiar <cerebrum.ch@gmail.com>
2017-12-17 01:31:35 +0700

    add add regexp to ip

--- web/httpHandle.go
+++ web/httpHandle.go
@@ -3,13 +3,14 @@ package web
 import (
 	"back-telega/model"
 	"back-telega/var"
-	"fmt"
-	"net/http"
-	"log"
 	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
-	"strings"
+	"regexp"
 	"strconv"
+	"strings"
 )
 
 func CountPoint(w http.ResponseWriter, r *http.Request) {
@@ -27,47 +28,48 @@ func CountPoint(w http.ResponseWriter, r *http.Request) {
 			log.Fatal(err)
 		}
 	}
-	jsonBlob, _:= json.Marshal(pointCount)
+	jsonBlob, _ := json.Marshal(pointCount)
 	fmt.Fprint(w, string(jsonBlob))
 }
 
 func InfoPoint(w http.ResponseWriter, r *http.Request) {
-	var  infoPoint lib.InfoPointJs
-	point :=  r.FormValue("point")
-	keys, err := lib.RedisDB.Keys(point+"_[iu][ps]*").Result()
+	var infoPoint lib.InfoPointJs
+	match, _ := regexp.Compile(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`)
+	point := r.FormValue("point")
+	keys, err := lib.RedisDB.Keys(point + "_[iu][ps]*").Result()
 	if err != nil {
 		fmt.Fprintf(w, "err get keys redis: %v", err)
 	}
 	vals, err := lib.RedisDB.MGet(keys...).Result()
 	if err != nil {
 		infoPoint.Success = false
-		jsonBlob, _ :=  json.Marshal(infoPoint)
+		jsonBlob, _ := json.Marshal(infoPoint)
 		fmt.Fprint(w, string(jsonBlob))
 		return
 	}
-	for i, val := range vals {
-		switch i {
-		case 0:
+	for _, val := range vals {
+		if match.MatchString(val.(string)) {
 			infoPoint.Ip = val.(string)
-		case 1:
+		} else {
 			infoPoint.UserAgent = val.(string)
+
 		}
 		infoPoint.Success = true
 	}
-	jsonBlob, _ :=  json.Marshal(infoPoint)
+	jsonBlob, _ := json.Marshal(infoPoint)
 	fmt.Fprint(w, string(jsonBlob))
 }
 
 func ListPoint(w http.ResponseWriter, r *http.Request) {
 	var pointList lib.PointList
-	keysIp , err := lib.RedisDB.Keys("[0-9]*_ip").Result()
+	keysIp, err := lib.RedisDB.Keys("[0-9]*_ip").Result()
 	if err != nil {
 		fmt.Fprintf(w, "err get keys; %v", err)
 	}
 	for _, keyIp := range keysIp {
 		point, _ := strconv.Atoi(strings.Trim(keyIp, "_ip"))
 		pointList.Point = append(pointList.Point, point)
-		}
-	jsonBlob, _:= json.Marshal(pointList)
+	}
+	jsonBlob, _ := json.Marshal(pointList)
 	fmt.Fprint(w, string(jsonBlob))
 }
